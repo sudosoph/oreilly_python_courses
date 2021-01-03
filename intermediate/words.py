31edba7cab07c4048dbdd2c875542eeecea34f9b
Sophia <soph@soph.codes>
2021-01-02 20:55:29 -0500

    Added function for words with all vowels

--- intermediate/words.py
+++ intermediate/words.py
@@ -1,17 +1,30 @@
 import scrabble
 import string
 
-#print all letters that never appear doubled
-for letter in string.ascii_lowercase:
-    exists = False
-    for word in scrabble.wordlist:
-        if letter * 2 in word:
-            exists = True
-            break
-    if not exists:
-        print("There are no English words with a double " + letter)
+vowels = 'aeiou'
 
-#print all words containing "uu"
+def has_all_vowels(word):
+    for vowel in vowels:
+        if vowel not in word:
+            return False
+    return True
+
+# print words that contain all vowels
 for word in scrabble.wordlist:
-    if "uu" in word:
+    if has_all_vowels(word):
         print(word)
+
+# #print all letters that never appear doubled
+# for letter in string.ascii_lowercase:
+#     exists = False
+#     for word in scrabble.wordlist:
+#         if letter * 2 in word:
+#             exists = True
+#             break
+#     if not exists:
+#         print("There are no English words with a double " + letter)
+
+# #print all words containing "uu"
+# for word in scrabble.wordlist:
+#     if "uu" in word:
+#         print(word)

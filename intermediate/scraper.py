db3ceda598d632e7d79e65429b134e4335bbc092
Sophia <soph@soph.codes>
2021-05-13 15:42:55 -0400

    Saved scraper images to directory

--- intermediate/scraper.py
+++ intermediate/scraper.py
@@ -7,13 +7,21 @@
 import urllib.request
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+import os
 
 # Download the index page
 base_url = "https://apod.nasa.gov/apod/archivepix.html"
 content = urllib.request.urlopen(base_url).read()
+download_directory = "apod_pictures"
 
 # For each link on the index page:
 for link in BeautifulSoup(content, "lxml").findAll("a"):
     print("Following link:", link)
     href = urljoin(base_url, link["href"])
-# Follow the link and pull down the image on that linked page
+    # Follow the link and pull down the image on that linked page
+    content = urllib.request.urlopen(href).read()
+    for img in BeautifulSoup(content, "lxml").findAll("img"):
+        img_href  =urljoin(href, img["src"])
+        print("Downloading image:", img_href)
+        img_name = img_href.split('/')[-1]
+        urllib.request.urlretrieve(img_href, os.path.join(download_directory, img_name))
